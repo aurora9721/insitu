79a961051d20bc02aba542305010399e1d842e2d
aurora9721 <55090088+aurora9721@users.noreply.github.com>
2021-12-09 13:13:33 +0100

    .

---
+++ script.js
@@ -0,0 +1,88 @@
+// function homeClick() {
+//     document.getElementById("img1").style.display = "block"
+//       document.getElementById("img2","img3","img4","img5").style.display = "none";
+// }
+
+function homeClick() {
+    document.getElementById("img1").style.display = "block"
+      document.getElementById("img2").style.display = "none"
+      document.getElementById("img3").style.display = "none"
+      document.getElementById("img4").style.display = "none"
+      document.getElementById("img5").style.display = "none";
+}
+
+
+
+// if (document.getElementById("img1").style.display = "block") {
+//   document.getElementById("img2","img3","img4","img5").style.display = "none";
+// }
+// else {
+//   document.getElementById("img1").style.display = "none"
+// }
+
+// function site1Click() {
+//     document.getElementById("img2").style.display = "block"
+//       document.getElementById("img1","img3","img4","img5").style.display = "none";
+// }
+
+function site1Click() {
+    document.getElementById("img2").style.display = "block"
+      document.getElementById("img1").style.display = "none"
+      document.getElementById("img3").style.display = "none"
+      document.getElementById("img4").style.display = "none"
+      document.getElementById("img5").style.display = "none";
+}
+
+// if (document.getElementById("img2").style.display = "block") {
+//   document.getElementById("img1","img3","img4","img5").style.display = "none";
+// }
+// else {
+//   document.getElementById("img2").style.display = "none"
+// }
+
+//
+// function site2Click() {
+//     document.getElementById("img3").style.display = "block"
+//       document.getElementById("img1","img2","img4","img5").style.display = "none";
+// }
+
+function site2Click() {
+    document.getElementById("img3").style.display = "block"
+      document.getElementById("img1").style.display = "none"
+      document.getElementById("img2").style.display = "none"
+      document.getElementById("img4").style.display = "none"
+      document.getElementById("img5").style.display = "none";
+}
+
+// if (document.getElementById("img3").style.display = "block") {
+//   document.getElementById("img1","img2","img4","img5").style.display = "none";
+// }
+// else {
+//   document.getElementById("img3").style.display = "none"
+// }
+
+// function site3Click() {
+//     document.getElementById("img4").style.display = "block"
+//       document.getElementById("img1","img2","img3","img5").style.display = "none";
+// }
+
+function site3Click() {
+    document.getElementById("img4").style.display = "block"
+      document.getElementById("img1").style.display = "none"
+      document.getElementById("img2").style.display = "none"
+      document.getElementById("img3").style.display = "none"
+      document.getElementById("img5").style.display = "none";
+}
+
+// function site4Click() {
+//     document.getElementById("img5").style.display = "block"
+//       document.getElementById("img1","img2","img3","img4").style.display = "none";
+// }
+
+function site4Click() {
+    document.getElementById("img5").style.display = "block"
+      document.getElementById("img1").style.display = "none"
+      document.getElementById("img2").style.display = "none"
+      document.getElementById("img3").style.display = "none"
+      document.getElementById("img4").style.display = "none";
+}
